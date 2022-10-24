35c4fd9d5f915c4709c011d73d724aebad0a6b24
Vitaliy <fireflier87@gmail.com>
2022-10-25 00:35:16 +0300

    loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -7,7 +7,7 @@ const Loader = () => {
       <RotatingLines
         strokeColor="#3f51b5"
         strokeWidth="5"
-        animationDuration="0.6"
+        animationDuration="0.75"
         width="96"
         visible={true}
       />
--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const LoaderContainer = styled.div`
-  margin: 0 auto;
+  margin: 20px auto;
 `;
