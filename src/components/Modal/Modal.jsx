8b1b5c278f1428069416ef02d6ad1bd9b4914f23
Vitaliy <fireflier87@gmail.com>
2022-10-30 03:02:03 +0200

    modal re

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,47 +1,45 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Backdrop, ModalContent } from './Modal.styled';
 import PropTypes from 'prop-types';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.onEscClick);
-  }
+const Modal = ({ onClose, largeImageURL, alt }) => {
+  useEffect(() => {
+    const onEscClick = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onEscClick);
-  }
+    window.addEventListener('keydown', onEscClick);
 
-  onEscClick = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
+    return () => {
+      window.removeEventListener('keydown', onEscClick);
+    };
+  }, [onClose]);
 
-  onBackdropClick = e => {
+  const onBackdropClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const { largeImageURL, alt } = this.props;
-    return createPortal(
-      <Backdrop onClick={this.onBackdropClick}>
-        <ModalContent>
-          <img src={largeImageURL} alt={alt} />
-        </ModalContent>
-      </Backdrop>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <Backdrop onClick={onBackdropClick}>
+      <ModalContent>
+        <img src={largeImageURL} alt={alt} />
+      </ModalContent>
+    </Backdrop>,
+    modalRoot
+  );
+};
 
 export default Modal;
 
 Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
   largeImageURL: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
 };
